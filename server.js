517d27219b2aa594e5cebe45da29e591e5c9d09c
safwankajas <84768183+safwankajas@users.noreply.github.com>
2021-07-27 22:48:58 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+ var express = require("express")
+ var app=express()
+ var http =require("http").createServer(app)
+ var io= require("socket.io")(http)
+
+
+ app.get("/",(req,res)=>{
+     res.sendFile(__dirname+"/public/board.html");
+ })
+
+ app.get("/admin",(req,res)=>{
+    res.sendFile(__dirname+"/public/admin.html");
+})
+io.on("connection",(socket)=>{
+    console.log("new connection established")
+    socket.on("disconnect",()=>{
+        console.log("connection closed")
+    })
+    socket.on('message',(msg)=>{
+        console.log(msg)
+        io.emit('board',msg)
+
+    })
+})
+
+http.listen(3000,()=>{
+    console.log("connected to server")
+})
